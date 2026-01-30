073e9fc6972f7e8ceb4842ee9f7ba31042bc4408
Jonathan <fuse.business@gmail.com>
2026-01-30 23:53:40 +0100

    Configure custom domain fusegg.net

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/net/', // Repository name
+    base: '/', // Custom domains serve from the root
 })
