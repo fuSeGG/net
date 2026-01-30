5c8e93206a36fe45792e20ed2675ab8db81e1f44
Jonathan <fuse.business@gmail.com>
2026-01-30 23:27:36 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="container">
+    <h1 class="glitch" data-text="fuSeGG.net">fuSeGG.net</h1>
+  </div>
+`
